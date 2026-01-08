4a672703bf5696d5fd0d311e2cefeab692001632
suchgamer <suchgamer91@gmail.com>
2026-01-08 21:01:14 +0900

    Fixed

--- アプリケーション起動.bat
+++ アプリケーション起動.bat
@@ -1,15 +1,11 @@
 @echo off
 echo "[JA] サーバーを起動しています..."
 echo "[EN] Server is starting..."
-cd server
-call npm run dev
-cd ..
+start "Server" cmd /k "cd server && npm run dev"
 
 echo "[JA] クライアントを起動しています..."
 echo "[EN] Client is starting..."
-cd client
-call npm run dev
-cd ..
+start "Client" cmd /k "cd client && npm run dev"
 
 echo "[JA] 無事アプリケーション起動しました! https://localhost:5173"
 echo "[EN] Application started successfully! https://localhost:5173"
