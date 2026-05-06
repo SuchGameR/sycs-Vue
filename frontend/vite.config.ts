1559d0621c2f67620f371849a29c0d78e4adbf2a
suchgamer <suchgamer91@gmail.com>
2026-05-07 00:21:33 +0900

    v1.2.20 Bit Style Fixed for Background Color Update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,10 @@ import vueDevTools from "vite-plugin-vue-devtools";
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_PAGES ? `/${process.env.REPO_NAME}/` : "/",
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    // vueDevTools()
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
